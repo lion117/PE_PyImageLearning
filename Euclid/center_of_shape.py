1c489edc56714f726396bb8b79b2c85548ea2535
hesiyuan <leonardohe@kugou.net>
2017-07-13 17:49:43 +0800

    add euclid recognize demo

---
+++ Euclid/center_of_shape.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+
+import os, sys
+import time
+
+# import the necessary packages
+import argparse
+import imutils
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+args = vars(ap.parse_args())
+
+# load the image, convert it to grayscale, blur it slightly,
+# and threshold it
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+
+cv2.imshow("Image", thresh)
+cv2.waitKey(2000)
+# find contours in the thresholded image
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+
+# loop over the contours
+for c in cnts:
+    # compute the center of the contour
+    M = cv2.moments(c)
+    try:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        # draw the contour and center of the shape on the image
+        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.putText(image, "center", (cX - 20, cY - 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        print("area:%0.2f Perimeter:%0.2f "%( cv2.contourArea(c), cv2.arcLength(c,True)))
+
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+    except ZeroDivisionError, e2:
+        print e2
+        # show the image
+
+
+
+
+if __name__ == "__main__":
+    print(os.path.abspath("__file__"))
+    print(os.getcwd())
